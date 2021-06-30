858bbd3b0995f35ed54af6f80f337d63ac0dbc74
Kristofers <krystoart@gmail.com>
2021-06-30 23:01:18 +0300

    Player: created consts for player

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ use macroquad::prelude::*;
 
 mod nodes;
 
+pub mod consts {
+    pub const RUN_SPEED: f32 = 300.0;
+}
+
 #[macroquad::main("Bomber")]
 async fn main() {
     loop {
--- src/nodes/player.rs
+++ src/nodes/player.rs
@@ -1,5 +1,8 @@
 use macroquad_platformer::Actor;
 
+use crate::{
+    consts,
+};
 
 pub struct Bomber {
     colider: Actor,
