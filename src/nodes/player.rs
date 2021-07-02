9a9546158d6708eb491152f0f8c3d0ac7cb958a2
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-07-03 01:05:38 +0300

    "Bomber" renamed to "player"

--- src/nodes/player.rs
+++ src/nodes/player.rs
@@ -11,17 +11,17 @@ use crate::{
     Resources,
 };
 
-pub struct Bomber {
+pub struct Player {
     pub colider: Actor,
     pos: Vec2,
     speed: Vec2,
 }
 
-impl Bomber {
-    pub fn new(spawner_pos: Vec2) -> Bomber {
+impl Player {
+    pub fn new(spawner_pos: Vec2) -> Self {
         let mut resources = storage::get_mut::<Resources>();
 
-        Bomber {
+        Self {
             colider: resources.physics.add_actor(spawner_pos, 30, 30),
             pos: spawner_pos,
             speed: vec2(0., 0.),
