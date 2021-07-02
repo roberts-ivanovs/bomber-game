f43d9bcb2a00f07f6228c18fb20d4bae91242210
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-07-03 01:18:54 +0300

    Basic (not working) resource struct

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use macroquad::prelude::*;
+use macroquad_tiled as tiled;
+use macroquad_platformer::World as CollisionWorld;
 
 mod nodes;
 
@@ -9,8 +11,50 @@ pub mod consts {
 }
 
 struct Resources {
-    bomber: Texture2D,
-    physics: World,
+    player: Texture2D,
+    tiled_map: tiled::Map,
+    collision_world: CollisionWorld,
+    bomb: Texture2D,
+}
+
+impl Resources {
+    async fn new() -> Result<Resources, macroquad::prelude::FileError> {
+
+        let bomb = load_texture("assets/items/bomb.png").await?;
+        let fire = load_texture("assets/items/fire.png").await?;
+        let player = load_texture("assets/items/player.png").await?;
+
+        let tileset = load_texture("assets/tileset.png").await?;
+        tileset.set_filter(FilterMode::Nearest);
+
+        let tiled_map_json = load_string("assets/map.json").await.unwrap();
+        let tiled_map = tiled::load_map(
+            &tiled_map_json,
+            &[("tileset.png", tileset)],
+            &[],
+        )
+        .unwrap();
+
+        let mut static_colliders = vec![];
+        for (_x, _y, tile) in tiled_map.tiles("main layer", None) {
+            static_colliders.push(tile.is_some());
+        }
+        let mut collision_world = CollisionWorld::new();
+        collision_world.add_static_tiled_layer(
+            static_colliders,
+            32.,
+            32.,
+            tiled_map.raw_tiled_map.width as _,
+            1,
+        );
+
+        Ok(Resources {
+            tiled_map,
+            collision_world,
+            bomb,
+            player,
+        })
+    }
 }
 
 #[macroquad::main("Bomber")]
