53de0201faacef523ee18da458077383cea20782
Kristofers <krystoart@gmail.com>
2021-07-03 00:23:28 +0300

    Player: created resources and new fn for player

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,22 @@ use macroquad::prelude::*;
 
 mod nodes;
 
+use macroquad_platformer::*;
+
 pub mod consts {
     pub const RUN_SPEED: f32 = 300.0;
+}
+
+struct Resources {
+    bomber: Texture2D,
+    physics: World,
 }
 
 #[macroquad::main("Bomber")]
 async fn main() {
     loop {
+        clear_background(BLACK);
+
+        next_frame().await;
     }
 }
--- src/nodes/player.rs
+++ src/nodes/player.rs
@@ -1,15 +1,30 @@
+use macroquad::prelude::*;
+
 use macroquad_platformer::Actor;
+
+use macroquad::experimental::{
+    collections::storage,
+};
 
 use crate::{
     consts,
+    Resources,
 };
 
 pub struct Bomber {
-    colider: Actor,
+    pub colider: Actor,
+    pos: Vec2,
+    speed: Vec2,
 }
 
 impl Bomber {
-    pub fn new() -> Bomber {
-        
+    pub fn new(spawner_pos: Vec2) -> Bomber {
+        let mut resources = storage::get_mut::<Resources>();
+
+        Bomber {
+            colider: resources.physics.add_actor(spawner_pos, 30, 30),
+            pos: spawner_pos,
+            speed: vec2(0., 0.),
+        }
     }
 }
