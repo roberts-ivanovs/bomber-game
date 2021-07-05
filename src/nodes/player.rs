a7e894de5826730d8a4c19c9232ba439e4cd3dca
Kristofers <krystoart@gmail.com>
2021-07-05 22:02:29 +0300

    Split off Bomber struct

--- src/nodes/player.rs
+++ src/nodes/player.rs
@@ -2,28 +2,94 @@ use macroquad::prelude::*;
 
 use macroquad_platformer::Actor;
 
-use macroquad::experimental::collections::storage;
+use macroquad::experimental::{collections::storage, scene::RefMut};
+
+use super::consts::RUN_SPEED;
 
 use crate::Resources;
 
-pub struct Player {
-    pub colider: Actor,
+pub struct Bomber {
+    pub collider: Actor,
     pos: Vec2,
     speed: Vec2,
 }
 
-impl Player {
+impl Bomber {
     pub fn new(spawner_pos: Vec2) -> Self {
         let mut resources = storage::get_mut::<Resources>();
 
         Self {
-            colider: resources.collision_world.add_actor(spawner_pos, 30, 30),
+            collider: resources.collision_world.add_actor(spawner_pos, 30, 30),
             pos: spawner_pos,
             speed: vec2(0., 0.),
+        }
+    }
+
+    pub fn draw(&mut self) {
+        let resources = storage::get::<Resources>();
+
+        let pos = resources.collision_world.actor_pos(self.collider);
+
+        draw_texture_ex(
+            resources.player,
+            pos.x - 16.,
+            pos.y,
+            WHITE,
+            DrawTextureParams {
+                source: Some(Rect::new(0.0, 0.0, 32., 32.)),
+                ..Default::default()
+            },
+        );
+    }
+}
+
+pub struct Player {
+    pub bomber: Bomber,
+}
+
+impl Player {
+    pub fn new(spawner_pos: Vec2) -> Self {
+        Player {
+            bomber: Bomber::new(spawner_pos),
         }
     }
 }
 
 impl scene::Node for Player {
-    // TODO
+    fn draw(mut node: RefMut<Self>) {
+        node.bomber.draw();
+        // let resources = storage::get_mut::<Resources>();
+
+        // let pos = resources.collision_world.actor_pos(node.collider);
+
+        // draw_texture_ex(
+        //     resources.player,
+        //     pos.x - 16.,
+        //     pos.y,
+        //     WHITE,
+        //     DrawTextureParams {
+        //         source: Some(Rect::new(0.0, 0.0, 32., 32.)),
+        //         ..Default::default()
+        //     },
+        // );
+    }
+
+    fn update(mut node: RefMut<Self>) {
+        let world = &mut storage::get_mut::<Resources>().collision_world;
+
+        if is_key_down(KeyCode::Right) {
+            node.bomber.speed.x = RUN_SPEED;
+        } else if is_key_down(KeyCode::Left) {
+            node.bomber.speed.x = -RUN_SPEED;
+        } else if is_key_down(KeyCode::Up) {
+            node.bomber.speed.y = RUN_SPEED;
+        } else if is_key_down(KeyCode::Down) {
+            node.bomber.speed.y = -RUN_SPEED;
+        } else {
+            node.bomber.speed.x = 0.;
+        }
+
+        world.move_h(node.bomber.collider, node.bomber.speed.x * get_frame_time());
+        world.move_v(node.bomber.collider, node.bomber.speed.y * get_frame_time());
+    }
 }
