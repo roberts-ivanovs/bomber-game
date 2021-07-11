8467925f4f55434c67b01d0058bca9bc615aa1ca
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-07-11 16:43:32 +0300

    Exports added to sapp_jsutils.d.ts

--- www/utils/sapp_jsutils.d.ts
+++ www/utils/sapp_jsutils.d.ts
@@ -1,5 +1,5 @@
 
-function js_object(obj: any): number;
-function consume_js_object(id: number): any;
-function get_js_object(id: number): any;
-function toUTF8Array(str: string): Array<string>;
+export function js_object(obj: any): number;
+export function consume_js_object(id: number): any;
+export function get_js_object(id: number): any;
+export function toUTF8Array(str: string): Array<string>;
