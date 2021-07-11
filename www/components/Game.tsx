9bd92a7084c5d43576c1344e531383bc141fc613
Roberts Ivanovs <robertsivanovs1999@gmail.com>
2021-07-11 13:49:32 +0300

    Frontend: Styled components support

--- www/components/Game.tsx
+++ www/components/Game.tsx
@@ -1,18 +1,49 @@
 import React from 'react'
 import { GlGames, GlLoader } from "../utils/GlLoader";
-import style from "./Game.module.scss";
+import styled, { css } from 'styled-components'
+
 
 interface Props {
   title: string;
   sourcePath: GlGames;
 }
 
+const Button = styled.button<{primary?: boolean}>`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+
+  ${props => props.primary && css`
+    background: palevioletred;
+    color: white;
+  `}
+`;
+
+const Container = styled.div`
+  text-align: center;
+`
+const Canvas = styled.canvas`
+margin: 0px;
+padding: 0px;
+width: 1280px;
+height: 720px;
+overflow: hidden;
+position: absolute;
+background: black;
+`
 
 export function Game({ title, sourcePath }: Props) {
   return (
     <article>
       <h2>{title}</h2>
-      <canvas className={style['canvas-game']} id="glcanvas" tabIndex={1}></canvas>
+      <Container>
+        <Button>Normal Button</Button>
+        <Button primary>Primary Button</Button>
+      </Container>
+      <Canvas id="glcanvas" tabIndex={1}/>
       <GlLoader sourcePath={sourcePath} />
     </article>
   )
--- www/pages/index.tsx
+++ www/pages/index.tsx
@@ -3,16 +3,27 @@ import React, { ReactElement } from 'react';
 
 import { Game } from '../components/Game';
 import { GlGames } from "../utils/GlLoader";
+import styled, { css } from 'styled-components'
 import { HeadFilled } from "./Head";
+
+const Container = styled.div<{ direction: 'row' |'column'}>`
+  display: flex;
+  justify-content: center;
+  ${props => css`
+    flex-direction: ${props.direction};
+  `}
+`
 
 export default function IndexPage(): ReactElement {
   return (
     <main>
       <HeadFilled />
-      <h1>Title</h1>
-      <section>
-        <Game sourcePath={GlGames.bomber} title="Ether Bomber" />
-      </section>
+      <Container direction={'column'}>
+        <h1>Title</h1>
+        <section>
+          <Game sourcePath={GlGames.bomber} title="Ether Bomber" />
+        </section>
+      </Container>
     </main>
   );
 }
